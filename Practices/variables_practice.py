750ca70c2830e9f1867420810399801cbc3ffc47
burntchocolatechip <israel.monge@ucas-edu.net>
2026-02-11 04:52:46 +0000

    variables

--- Practices/variables_practice.py
+++ Practices/variables_practice.py
@@ -7,3 +7,17 @@ number = 5
 big_number = 800
 
 breakfast = "cereal"
+
+color = "yellow"
+
+school_name = "Ucas"
+
+year = 2026
+
+eye_color = "brown"
+
+someones_age = 15
+
+favorite_subject = "geography"
+
+print(name, number, big_number, breakfast, color, school_name, year, eye_color, someones_age, favorite_subject)
