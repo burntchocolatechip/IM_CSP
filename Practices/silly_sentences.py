5d1f9fd767f8c6fcf62a063a78a5374410f48125
burntchocolatechip <israel.monge@ucas-edu.net>
2026-02-12 07:11:19 +0000

    silly

--- Practices/silly_sentences.py
+++ Practices/silly_sentences.py
@@ -1,2 +1,11 @@
 # IM Silly Sentences
 
+name = input("type a name: ")
+
+place = input("type a place: ")
+
+verb = input("type a past tense verb: ")
+
+noun = input("type a noun: ")
+
+print(name, "went to the",place, "where they",verb, "and bought a", noun, ".")
