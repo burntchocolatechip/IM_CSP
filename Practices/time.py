84f53e0e3b6e6509a7a2abca62aa0c63f7f5481d
burntchocolatechip <israel.monge@ucas-edu.net>
2026-02-12 07:21:20 +0000

    time

--- Practices/time.py
+++ Practices/time.py
@@ -2,7 +2,11 @@
 
 time = int(input("time of day in military time: "))
 
-if time < 0000 and time > 1100:
+if time <= 12:
     print("goodmorning!")
-elif (time) < 1100
-    print("")
+elif time <= 18:
+    print("good afteernoon!")
+elif time <= 20:
+    print("good evening")
+else:
+    print("good night!")
