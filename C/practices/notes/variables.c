a8cb399cd56867d33089e0a6a9cea928c6da2a85
burntchocolatechip <israel.monge@ucas-edu.net>
2026-02-18 14:42:22 -0700

    variables

---
+++ C/practices/notes/variables.c
@@ -0,0 +1,16 @@
+// This is a comment in C!
+#include <stdio.h> // Lets us use inputs and outputs
+
+int main(){
+    //variables examples
+    int age = 9999;
+    float gpa = 3.98;
+    char grade = 'A'; // single letter uses sinle quotes
+    char name[] = "Eric";
+
+    printf("It worked!\n");
+    printf("%d", age);
+    printf("Your GPA is: %f\n", gpa);
+    printf("You have a %c in computer scinece\n", grade);
+    return 0; // required last line
+}
