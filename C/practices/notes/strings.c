b1b2d05475be02cf7862c5fb389de485b982a519
burntchocolatechip <israel.monge@ucas-edu.net>
2026-02-24 14:42:11 -0700

    strings

---
+++ C/practices/notes/strings.c
@@ -0,0 +1,39 @@
+// IM strings Notes
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char subject[] = "Computer Science Principles";
+    char school[] = "Ucas";
+    char book[50];
+    
+    printf("What is you favorite book: ");
+    //scanf("%s", &book);
+    fgets(book, sizeof(book), stdin);
+
+    printf("This is %s at %s. It is awesome!\n", subject, school);
+    printf("%sIs a cool book!\n", book);
+
+    //Concatenation
+    char first[] = "Vienna";
+    char last[] = "LaRose";
+    char full_name[20];
+
+    first[0] = 'k';
+    first[1] = 'a';
+    first[2] = 't';
+    first[3] = 'i';
+    first[4] = 'e';
+    first[5] = ' ';
+
+    strcat(full_name, first);
+    strcat(full_name, " ");
+    strcat(full_name, last);
+
+    printf("%s\n", full_name);
+
+    //length of string
+    printf("%lu\n", strlen(full_name));
+
+    return 0;
+}
