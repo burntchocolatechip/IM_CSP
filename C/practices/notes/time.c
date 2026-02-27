057ff2b9e4086fad2148668764a197507812db12
burntchocolatechip <israel.monge@ucas-edu.net>
2026-02-27 14:41:04 -0700

    time

--- C/practices/notes/time.c
+++ C/practices/notes/time.c
@@ -1,7 +1,14 @@
-// IM time of day
+// IM time
 #include <stdio.h>
 
 int main(){
 
+    int time = 0000;
+
+    if (time > 0000){
+        printf("Good Morning!\n");
+    }else if (time >= 0000){
+        printf("Good Morning!\n");
+    }
     return 0;
 }
