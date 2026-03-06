9c1371162878bf15334ef7ffb0db6471be559773
burntchocolatechip <israel.monge@ucas-edu.net>
2026-03-06 05:08:20 +0000

    variables

--- C/practices/notes/variables_practice.c
+++ C/practices/notes/variables_practice.c
@@ -2,5 +2,35 @@
 #include <stdio.h>
 
 int main(){
-    
+    char name[] = "Israel Monge";
+    printf("%s\n", name);
+
+    char lowNumber[] = "8";
+    printf("%s\n", lowNumber );
+
+    char gpa[] = "3";
+    printf("%s\n", gpa);
+
+    char breakfast[] = "cereal";
+    printf("%s\n", breakfast);
+
+    char favoriteColor[] = "red";
+    printf("%s\n", favoriteColor);
+
+    char school[] = "UCAS";
+    printf("%s\n", school);
+
+    char year[] = "2024";
+    printf("%s\n", year); 
+
+    char eyeColor[] = "brown";
+    printf("%s\n", eyeColor);
+
+    char age[] = "16";
+    printf("%s\n", age);
+
+    char favoriteSubject[] = "reading";
+    printf("%s\n", favoriteSubject);
+
+    return 0;
 }
